a163c72630587b2439160ca53df0145537ede728
thomasfouan <thomasfouan@hotmail.fr>
2017-05-26 15:59:37 +0200

    factory updated for areas

--- ElectionLibrary/Environment/AbstractArea.cs
+++ ElectionLibrary/Environment/AbstractArea.cs
@@ -5,10 +5,9 @@ namespace ElectionLibrary.Environment
 {
     public abstract class AbstractArea : AbstractLibrary.Environment.AbstractArea
     {
-
         public Position position { get;}
 
-        public AbstractArea(string name, Position position) : base(name)
+        protected AbstractArea(string name, Position position) : base(name)
         {
             this.position = position;
         }
---
+++ ElectionLibrary/Environment/Street.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace ElectionLibrary.Environment
+{
+    public class Street : AbstractArea
+    {
+        public Street(string name, Position position) : base(name, position)
+        {
+        }
+    }
+}
