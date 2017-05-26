7199a90c359feb25b0ed1e0de9872e0b0341e65a
Vincent Rossignol <rossignol.vince@gmail.com>
2017-05-26 10:16:30 +0200

    New simulation load CSV

--- ElectionSimulator/ElectionSimulator/MainWindow.xaml.cs
+++ ElectionSimulator/ElectionSimulator/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.VisualBasic.FileIO;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
@@ -24,30 +25,96 @@ namespace ElectionSimulator
     {
         DispatcherTimer dt = new DispatcherTimer();
         Stopwatch sw = new Stopwatch();
+        /*List<BitmapImage> buildings = new List<BitmapImage>(new BitmapImage[] {
+            BitmapImage Building1 = new BitmapImage(new Uri("resource/building1.png", UriKind.Relative)),
+            BitmapImage Building2 = new BitmapImage(new Uri("resource/building2.png", UriKind.Relative)),
+            BitmapImage Building3 = new BitmapImage(new Uri("resource/building3.png", UriKind.Relative))
+        });*/
 
         public MainWindow()
         {
             InitializeComponent();
             DataContext = App.ElectionVM;
         }
+        []
+        private void InitBoard()
+        {
+            Board.ColumnDefinitions.Clear();
+            Board.RowDefinitions.Clear();
+            Board.Children.Clear();
+
+            for (int i = 0; i < App.ElectionVM.DimensionX; i++)
+            {
+                Board.ColumnDefinitions.Add(new ColumnDefinition());
+            }
+
+            for (int i = 0; i < App.ElectionVM.DimensionY; i++)
+            {
+                Board.RowDefinitions.Add(new RowDefinition());
+            }
+        }
 
         private void StartNewSimulation(object sender, RoutedEventArgs e)
         {
-            Board.Background = new SolidColorBrush(Color.FromArgb(255, 0, 100, 100));
-            /*using (TextFieldParser parser = new TextFieldParser(@"c:\temp\test.csv"))
+            string MapFile = GetMapFile();
+            if (MapFile != null)
+            {
+                List<List<string>> Map = LoadMap(MapFile);
+                App.ElectionVM.DimensionX = Map[0].Count;
+                App.ElectionVM.DimensionY = Map.Count;
+                InitBoard();
+                LoadTextures(Map);
+            }
+        }
+
+        private void LoadTextures(List<List<string>> map)
+        {
+            
+        }
+
+        private List<List<string>> LoadMap(string MapFile)
+        {
+            List<List<string>> Map = new List<List<string>>();
+            using (TextFieldParser parser = new TextFieldParser(MapFile))
             {
                 parser.TextFieldType = FieldType.Delimited;
-                parser.SetDelimiters(",");
+                parser.SetDelimiters(";");
+                int i = 0;
                 while (!parser.EndOfData)
                 {
-                    //Process row
                     string[] fields = parser.ReadFields();
+                    Map.Add(new List<string>());
                     foreach (string field in fields)
                     {
-                        //TODO: Process field
+                        Map[i].Add(field);
+                        Console.Write(field);
                     }
+                    Console.WriteLine();
+                    i++;
                 }
-            }*/
+            }
+            return Map;
+        }
+
+        private string GetMapFile()
+        {
+            // Create OpenFileDialog 
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+
+            // Set filter for file extension and default file extension 
+            dlg.DefaultExt = ".csv";
+            dlg.Filter = "CSV Files (*.csv)|*.csv";
+
+            // Display OpenFileDialog by calling ShowDialog method 
+            Nullable<bool> result = dlg.ShowDialog();
+
+            // Get the selected file name and display in a TextBox 
+            if (result == true)
+            {
+                return dlg.FileName;
+            }
+
+            return null;
         }
     }
 }
