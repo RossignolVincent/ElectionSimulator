a1a61993f295fdbe089df739b0bdd30b7f405317
robinfruitet <rfruitet@gmail.com>
2017-05-27 10:17:42 +0200

    Political parties

---
+++ ElectionLibrary/Parties/Concrete/LR.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElectionLibrary.Parties.Concrete
+{
+    public class LR : PoliticalParty
+    {
+
+        public override PoliticalParty Instance()
+        {
+            if (instance == null)
+            {
+                instance = new LR();
+                name = "Les Républicains";
+            }
+
+            return instance;
+        }
+    }
+}
---
+++ ElectionLibrary/Parties/PoliticalParty.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace ElectionLibrary.Parties
+{
+    public abstract class PoliticalParty
+    {
+        public static string name { get; set; }
+        protected PoliticalParty instance;
+
+        protected PoliticalParty()
+        {
+        }
+
+        public abstract PoliticalParty Instance();
+    }
+}
